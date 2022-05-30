ce1ad8e380978eff1be15a5c2c404d3d401c0c68
ghouilimen <65468597+ghouilimen@users.noreply.github.com>
2022-05-30 14:41:48 +0100

    finfim

--- force-app/main/default/lwc/auditNav/auditNav.js
+++ force-app/main/default/lwc/auditNav/auditNav.js
@@ -4,54 +4,58 @@ export default class AuditNav extends NavigationMixin(LightningElement) {
 
     navigateToAuditContact(){
         this[NavigationMixin.Navigate]({
-            type:"standard__webPage",
+            type:"standard__navItemPage",
             attributes:{
-                //objectApiName: 'AuditContact__c',
+                apiName: 'contact_audit_details',
                 //actionName: 'home'
-                url: 'https://wise-wolf-n2hypj-dev-ed.lightning.force.com/lightning/n/Contact_audit_details'
-
+                //url: 'https://wise-wolf-n2hypj-dev-ed.lightning.force.com/lightning/n/Contact_audit_details'
+               
+              
             }
         })
     }
     navigateToAuditAccount(){
         this[NavigationMixin.Navigate]({
-            type:'standard__webPage',
+            type:'standard__navItemPage',
             attributes:{
-                //objectApiName: 'AuditAccount__c',
-                // actionName: 'home'
-                url: "https://wise-wolf-n2hypj-dev-ed.lightning.force.com/lightning/n/Account_audit",
+               apiName: 'Account_audit',
+                
             }
         })
     }
     navigateToAuditOpportunity(){
         this[NavigationMixin.Navigate]({
-            type:'standard__webPage',
+            type:'standard__navItemPage',
             attributes:{
-                url:"https://wise-wolf-n2hypj-dev-ed.lightning.force.com/lightning/n/Opportunity_audit_details"
+                //url:"https://wise-wolf-n2hypj-dev-ed.lightning.force.com/lightning/n/Opportunity_audit_details"
+                apiName: 'Opportunity_audit_details',
             }
         })
     }
     navigateToAuditProduct(){
         this[NavigationMixin.Navigate]({
-            type:'standard__webPage',
+            type:'standard__navItemPage',
             attributes:{
-               url:"https://wise-wolf-n2hypj-dev-ed.lightning.force.com/lightning/n/Porduct_audit_details"
+               //url:"https://wise-wolf-n2hypj-dev-ed.lightning.force.com/lightning/n/Porduct_audit_details"
+               apiName: 'Porduct_audit_details',
             }
         })
     }
     navigateToAuditContract(){
         this[NavigationMixin.Navigate]({
-            type:'standard__webPage',
+            type:'standard__navItemPage',
             attributes:{
-               url:"https://wise-wolf-n2hypj-dev-ed.lightning.force.com/lightning/n/Contract_audit_details"
+              // url:"https://wise-wolf-n2hypj-dev-ed.lightning.force.com/lightning/n/Contract_audit_details"
+              apiName: 'Contract_audit_details' ,
             }
         })
     }
     navigateToAuditLead(){
         this[NavigationMixin.Navigate]({
-            type:'standard__webPage',
+            type:'standard__navItemPage',
             attributes:{
-               url:"https://wise-wolf-n2hypj-dev-ed.lightning.force.com/lightning/n/Lead_audit_details"
+              // url:"https://wise-wolf-n2hypj-dev-ed.lightning.force.com/lightning/n/Lead_audit_details"
+              apiName: 'Lead_audit_details' ,
             }
         })
     }
